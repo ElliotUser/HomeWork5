175bfb82c5193469934d8e4e293eb78e16af2e15
ElliotUser <andersen.e@bk.ru>
2020-04-19 08:59:47 +0300

    HomeWork_5

--- src/ru/rickSanchez/company/homework_5/Main.java
+++ src/ru/rickSanchez/company/homework_5/Main.java
@@ -7,31 +7,21 @@ public class Main {
     static float[] arr = new float[size];
 
     public static void main(String[] args) {
-        //Отличие первого метода от второго:
-        //Первый просто бежит по массиву и вычисляет значения.
         arrCalculation(arr);
-        //Второй разбивает массив на два массива, в двух потоках
-        // высчитывает новые значения и потом склеивает эти массивы
-        // обратно в один.
         splitArray(arr);
     }
 
     public static void splitArray(float[] array) {
         long before = 0;
-        //2) Заполняем этот массив единицами:
         for(int i = 0; i < array.length; i++) {
             array[i] = 1;
         }
-        //3) Засекаем время выполнения:
         before = System.currentTimeMillis();
-            //4)Разбиваем массив на два массива, в двух потоках
-            // высчитывает новые значения и потом склеивает эти массивы
-            // обратно в один.
             float[] arrSplit_a = new float[halfSize];
             float[] arrSplit_b = new float[halfSize];
-            //первый массив
+
             System.arraycopy(array,0,arrSplit_a,0,halfSize);
-            //второй массив
+
             System.arraycopy(array,halfSize,arrSplit_b,0,halfSize);
 
             Thread thread_1 = new Thread(new Runnable() {
@@ -40,13 +30,9 @@ public class Main {
                     for(int j = 0; j < arrSplit_a.length; j++) {
                         arrSplit_a[j] = (float)(array[j] * Math.sin(0.2f + j / 5) * Math.cos(0.2f + j / 5) * Math.cos(0.4f + j / 2));
                     }
+                    System.out.println("Поток №1 закончил работу.");
                 }
             });
-        try {
-            thread_1.join();
-        } catch(InterruptedException e) {
-            e.printStackTrace();
-        }
 
         Thread thread_2 = new Thread(new Runnable() {
                 @Override
@@ -54,20 +40,21 @@ public class Main {
                     for(int j = 0; j < arrSplit_a.length; j++) {
                         arrSplit_a[j] = (float)(array[j] * Math.sin(0.2f + j / 5) * Math.cos(0.2f + j / 5) * Math.cos(0.4f + j / 2));
                     }
+                    System.out.println("Поток №2 закончил работу.");
                 }
             });
+
+        thread_1.start();
+        thread_2.start();
         try {
+            thread_1.join();
             thread_2.join();
         } catch(InterruptedException e) {
             e.printStackTrace();
         }
+        System.arraycopy(arrSplit_a,0,array,0,halfSize);
+        System.arraycopy(arrSplit_b,0,array,halfSize,halfSize);
 
-
-        //склеиваем обратно
-            System.arraycopy(arrSplit_a,0,array,0,halfSize);
-            System.arraycopy(arrSplit_b,0,array,halfSize,halfSize);
-
-        //6) В консоль выводим время работы:
         System.out.println("Время выполнения метода splitArray() = " + (System.currentTimeMillis()-before) + " мс.");
     }
 
@@ -76,19 +63,13 @@ public class Main {
 
     public static void arrCalculation(float[] array){
         long before = 0;
-        //2) Заполняем этот массив единицами:
         for(int i = 0; i < array.length; i++) {
             array[i] = 1;
         }
-        //3) Засекаем время выполнения:
         before = System.currentTimeMillis();
-        //4) Проходим по всему массиву и для каждой ячейки считаем новое
-        //значение по формуле:
         for(int i = 0; i < array.length; i++) {
             array[i] = (float)(array[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
-        //5) Проверяется время окончания метода:
-        //6) В консоль выводим время работы:
         System.out.println("Время выполнения метода arrCalculation() = " + (System.currentTimeMillis()-before) + " мс.");
     }
 }
